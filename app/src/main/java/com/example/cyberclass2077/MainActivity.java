53fa8b0fb7dfd6dfeb35f3ca38f32199d7de4ae5
李林洁 <Kyle@lilinjiedeMacBook-Pro-2.local>
2019-06-26 10:33:35 +0800

    start

---
+++ app/src/main/java/com/example/cyberclass2077/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.cyberclass2077;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
+}
