d5bb538f1e87af06359513ce27f909203de6b96d
Kyle <1607734997@qq.com>
2019-07-03 15:37:07 +0800

    继续跳，这次交给vvg学跳跃

---
+++ app/src/main/java/com/example/cyberclass2077/views/Fragment3UploadActivity.java
@@ -0,0 +1,12 @@
+package com.example.cyberclass2077.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragment3UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
