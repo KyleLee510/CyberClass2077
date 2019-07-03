72e9afbc3fad08d26d34cb1902cac0fb271ae577
cysisu <cysisu@DESKTOP-EI9TEGS>
2019-07-03 16:47:38 +0800

    cy

--- app/src/main/java/com/example/cyberclass2077/views/Fragment3SettingActivity.java
+++ app/src/main/java/com/example/cyberclass2077/views/Fragment3SettingActivity.java
@@ -10,9 +10,8 @@ import android.widget.ImageButton;
 import com.example.cyberclass2077.R;
 
 public class Fragment3SettingActivity extends AppCompatActivity {
-    private ImageButton backButton;
-    private ConstraintLayout to_changePassword;
 
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +24,6 @@ public class Fragment3SettingActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent(Fragment3SettingActivity.this, MainActivity.class);
                 intent.putExtra("fragment",2);
-                startActivity(intent);
-                finish();
-                //需要在finish和startActivity之后进行
-                //第一个参数是需要打开的Activity进入时的动画，第二个是需要关闭的Activity离开时的动画
-                overridePendingTransition(R.anim.anim_slide_from_right, R.anim.anim_slide_from_right);
-            }
-        });
-        to_changePassword = (ConstraintLayout) findViewById(R.id.constraintLayout_change_password);
-        to_changePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Fragment3SettingActivity.this, ChangePasswordActivity.class);
                 startActivity(intent);
                 finish();
                 //需要在finish和startActivity之后进行
