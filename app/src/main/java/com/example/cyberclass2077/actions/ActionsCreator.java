31cb0e7d7ae2593511292615070889702b553bc5
LeoneKuma <leonekuma@gmailcom>
2019-07-04 16:14:19 +0800

    完成了重名检测除Activity以外的工作

--- app/src/main/java/com/example/cyberclass2077/actions/ActionsCreator.java
+++ app/src/main/java/com/example/cyberclass2077/actions/ActionsCreator.java
@@ -16,6 +16,8 @@ public class ActionsCreator {
         }
         return instance;
     }
+
+    //登录请求
     public void login(String userName,String passWord){
         User user = new User();
         user.setUserName(userName);
@@ -23,20 +25,28 @@ public class ActionsCreator {
 
         dispatcher.dispatch(new LoginAction(LoginAction.ACTION_LOGIN,user));
     }
+    //注册请求
     public void signup(String userName,String passWord){
         User user = new User();
         user.setUserName(userName);
         user.setPassWord(passWord);
         dispatcher.dispatch(new SignupAction(SignupAction.ACTION_SIGNUP,user));
     }
+    //用户名查重请求
+    public void nameCheck(String userName){
+        dispatcher.dispatch(new UserNameCheckAction(UserNameCheckAction.ACTION_USERNAMECHECK,userName));
+    }
+    //登出请求
     public void logout(String userName){
         dispatcher.dispatch(new LogoutAction(LogoutAction.ACTION_LOGOUT,userName));
     }
+    //更新（修改）密码请求
     public void updatePassword(String userName,String passWord){
         User user = new User();
         user.setUserName(userName);
         user.setPassWord(passWord);
         dispatcher.dispatch(new UpdatePasswordAction(UpdatePasswordAction.ACTION_UPDATE_PASSWORD,user));
     }
+
 
 }
---
+++ app/src/main/java/com/example/cyberclass2077/actions/UserNameCheckAction.java
@@ -0,0 +1,6 @@
+package com.example.cyberclass2077.actions;
+
+public class UserNameCheckAction extends Action<String> {
+    public static final String ACTION_USERNAMECHECK="username_check";
+    UserNameCheckAction(String type,String userName){super(type,userName);}
+}
