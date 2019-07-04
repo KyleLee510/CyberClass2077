043fbf230491c62a7186d0414e6b28d3c09f85e4
LeoneKuma <leonekuma@gmailcom>
2019-07-04 15:25:51 +0800

    增加了新的用户信息类，用于设置界面 包括：  用户名（不可修改，用户登录用）  昵称  性别  头像(路径)  出生日期（YYYY-MM-DD格式，字符串）  关注的人的数量  粉丝的数量

---
+++ app/src/main/java/com/example/cyberclass2077/model/UserInfo.java
@@ -0,0 +1,70 @@
+package com.example.cyberclass2077.model;
+
+public class UserInfo {
+
+    private String userName;
+    private String nickName;
+    private String gender;
+    private String portrait;
+    private String birthDate;
+    private Integer followeeNum;
+    private Integer followerNum;
+
+    public UserInfo(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPortrait() {
+        return portrait;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getFolloweeNum() {
+        return followeeNum;
+    }
+
+    public Integer getFollowerNum() {
+        return followerNum;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setPortrait(String portrait) {
+        this.portrait = portrait;
+    }
+
+    public void setFolloweeNum(Integer followeeNum) {
+        this.followeeNum = followeeNum;
+    }
+
+    public void setFollowerNum(Integer followerNum) {
+        this.followerNum = followerNum;
+    }
+}
