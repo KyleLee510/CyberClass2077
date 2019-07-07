56276f39633f5e9194f0d376b706e01b6958c445
cysisu <2461900466@qq.com>
2019-07-07 15:45:41 +0800

    陈勇-2019-7-7

---
+++ app/src/main/java/com/example/cyberclass2077/views/Dynamic/DynamicAttentionFragment.java
@@ -0,0 +1,39 @@
+package com.example.cyberclass2077.views.Dynamic;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.cyberclass2077.R;
+import com.example.cyberclass2077.adapter.DynamicAdapter;
+import com.example.cyberclass2077.bean.DynamicBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DynamicAttentionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.dynamic_attention_layout, container, false);
+        ListView listView=(ListView)view.findViewById(R.id.id_dynamic_attention);
+
+        List<DynamicBean> dynamicBeanList=new ArrayList<>();
+        for(int i=0;i<10;i++)
+        {
+            DynamicBean dynamicBean=new DynamicBean();
+            dynamicBeanList.add(dynamicBean);
+        }
+
+
+        listView.setAdapter(new DynamicAdapter(getActivity(),dynamicBeanList));
+        return view;
+    }
+}
