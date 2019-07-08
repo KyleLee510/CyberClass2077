2784eac17e0e5d188513ae0793dd20414c0c6b13
Kyle <1607734997@qq.com>
2019-07-08 14:12:15 +0800

    签到和勇哥需求

--- app/src/main/java/com/example/cyberclass2077/views/Dynamic/DynamicPublish.java
+++ app/src/main/java/com/example/cyberclass2077/views/Dynamic/DynamicPublish.java
@@ -1,21 +1,34 @@
 package com.example.cyberclass2077.views.Dynamic;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.cyberclass2077.R;
+import com.example.cyberclass2077.pictureselector.Constant;
+import com.example.cyberclass2077.pictureselector.ImageUtils;
+import com.example.cyberclass2077.pictureselector.PictureSelector;
+import com.example.cyberclass2077.views.UserDataSettingActivity;
 
 public class DynamicPublish extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView ibtn;
+    private ImageView imageView_choose;
+    private Button btn_publish;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dynamic_pulish);
 
         //返回
-        ImageButton ibtn=findViewById(R.id.dyname_publish_back_button);
+        ibtn = findViewById(R.id.dyname_publish_back_button);
         ibtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,9 +36,43 @@ public class DynamicPublish extends AppCompatActivity implements View.OnClickLis
             }
         });
 
-        //发布
+        //图片选择
+        imageView_choose = findViewById(R.id.imageView_choose);
+        imageView_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PictureSelector
+                        .create(DynamicPublish.this, PictureSelector.SELECT_REQUEST_CODE)
+                        .selectPicture(true, 500, 500, 1, 1);
+            }
+        });
+
+        //发表
+        btn_publish = findViewById(R.id.btn_publish);
+        btn_publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //该函数负责存储用户要使用的图像，第一个参数从临时保存的地方提取图片信息，第二个参数是设置的保存路径，第三个参数设置文件名
+                ImageUtils.saveFileToJPEG(BitmapFactory.decodeFile(Constant.TEMP_PICTUREPATH), Constant.USERPHOTO_PATH, "test");//缓冲用户头像至本地
+            }
+        });
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /*结果回调*/
+        if (requestCode == PictureSelector.SELECT_REQUEST_CODE) {
+            if (data != null) {
+                String picturePath = data.getStringExtra(PictureSelector.PICTURE_PATH);
+                imageView_choose.setImageBitmap(BitmapFactory.decodeFile(picturePath)); //图片加载
+                imageView_choose.setScaleType(ImageView.ScaleType.FIT_XY);
+            }
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
