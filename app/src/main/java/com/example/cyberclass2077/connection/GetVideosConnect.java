a048a9fd90615fded507615dd803ad149aba6562
LeoneKuma <leonekuma@gmailcom>
2019-07-10 16:36:47 +0800

    获取视频列表功能添加了获得头像功能

--- app/src/main/java/com/example/cyberclass2077/connection/GetVideosConnect.java
+++ app/src/main/java/com/example/cyberclass2077/connection/GetVideosConnect.java
@@ -1,5 +1,8 @@
 package com.example.cyberclass2077.connection;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.alibaba.fastjson.JSON;
 import com.example.cyberclass2077.config.AppConfig;
 import com.example.cyberclass2077.model.FileInfo;
@@ -8,6 +11,8 @@ import com.example.cyberclass2077.stores.UserStore;
 import com.squareup.okhttp.MultipartBuilder;
 import com.squareup.okhttp.RequestBody;
 
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -44,6 +49,14 @@ public class GetVideosConnect extends Connect<Map> {
         String video_list_str=(String)resultMap.get("videoList");
         String video_url_list_str=(String)resultMap.get("videoUrlList");
         String video_like_list_str=(String)resultMap.get("videoLikeList");
+        String portraitsStr=(String)resultMap.get("portraitsList");
+        List<String> video_portraits_str=JSON.parseArray(portraitsStr,String.class);
+        List<Bitmap> portraitsList=new ArrayList<Bitmap>();
+        for (String portraitStr:video_portraits_str
+        ) {
+            portraitsList.add(stringToBitmap(portraitStr));
+
+        }
         List<FileInfo> video_list=JSON.parseArray(video_list_str,FileInfo.class);
         List<String> video_url_list=JSON.parseArray(video_url_list_str,String.class);
         List<Boolean>video_like_list=JSON.parseArray(video_like_list_str,Boolean.class);
@@ -54,10 +67,25 @@ public class GetVideosConnect extends Connect<Map> {
                         isGetVideosSuccessful,
                         video_list,
                         video_url_list,
-                        video_like_list
+                        video_like_list,
+                        portraitsList
                 )
         );
         store.emitStoreChange();
 
+    }
+    public Bitmap stringToBitmap(String string) {
+        // 将字符串转换成Bitmap类型
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            //bitmapArray = Base64.decode(string, Base64.DEFAULT);
+            bitmapArray= Base64.getDecoder().decode(string);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0,
+                    bitmapArray.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
